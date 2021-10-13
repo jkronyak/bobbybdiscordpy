5e663380a03975fb5e4a498b510deab0139ff7ce
jkronyak <56709613+jkronyak@users.noreply.github.com>
2021-10-12 21:37:43 -0400

    Basic functions implemented. Bot can respond to messages containing bobbyb or bobby b

--- bobbyb.py
+++ bobbyb.py
@@ -1,23 +1,27 @@
+# Author: Jared Anthony Kronyak
+# This is my simple attempt at converting my old Bobby B Discord bot from Java
+# to Python, being much more concise.
 
-import os
 import discord
 import random
 import config
+import logging
 
 client = discord.Client()
 quotes = []
-
+logging.basicConfig(level=logging.INFO)
 
 async def on_ready():
     print('Logged in as  {0.user}'.format(client))
 
 
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().contains('bobby b') or message.content.lower().contains('bobbyb'):
-        await message.channel.send(         )
+    if 'bobby b' in message.content.lower() or 'bobbyb' in message.content.lower():
+        await message.channel.send(get_random_quote())
 
 
 def load_quotes():
@@ -28,15 +32,12 @@ def load_quotes():
 
 
 def get_random_quote():
-    return quotes[random.randint(0, len(quotes))]
+    return quotes[random.randint(0, len(quotes) - 1)]
 
 
 if __name__ == '__main__':
     try:
-        #client.run(config.token)
         quotes = load_quotes()
-
-
-
+        client.run(config.token)
     except Exception as e:
-        print(e)
+        print(e.with_traceback())
