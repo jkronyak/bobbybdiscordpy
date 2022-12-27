8e1917301d034c4b21bc3ef8caf52c6f592d7ce8
jkronyak <56709613+jkronyak@users.noreply.github.com>
2022-12-27 15:55:14 -0500

    Updated with intents; .env file

--- bobbyb.py
+++ bobbyb.py
@@ -4,27 +4,29 @@
 
 import discord
 import random
-import config
+import os
 import logging
+import dotenv
+dotenv.load_dotenv(os.path.join(os.getcwd(), '.env'))
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=discord.Intents.all())
 quotes = []
 logging.basicConfig(level=logging.INFO)
 
 
 async def on_ready():
     print('Logged in as  {0.user}'.format(client))
-
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
     if 'booby b' in message.content.lower() or 'boobyb' in message.content.lower():
         await message.channel.send("THANK THE GODS FOR BESSIE AND HER TITS!")
     elif 'bobby b' in message.content.lower() or 'bobbyb' in message.content.lower() \
-            or client.user in message.mentions:
+            or client.user in message.mentions or message.guild.get_role(712425928462041188) in message.role_mentions:
         await message.channel.send(get_random_quote())
 
 
@@ -42,6 +44,6 @@ def get_random_quote():
 if __name__ == '__main__':
     try:
         quotes = load_quotes()
-        client.run(config.token)
+        client.run(os.getenv("token"))
     except Exception as e:
-        print(e.with_traceback())
+        print(e)
